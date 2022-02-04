c6d5c3904068de10be2cceb1de52f09e333c16ba
Carol Alonso <lacerdacarol01@gmail.com>
2022-02-03 21:41:33 -0300

    Exercícios do dia 1 ao 3 de fevereiro

---
+++ 1-3fev_Herança, Coleção, Exceção/Excecao.java
@@ -0,0 +1,30 @@
+package T44_5;
+
+public class Excecao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String frase = null;
+		String novaFrase = null;
+		
+		try // faz uma tentativa de execução para verificar o erro
+		{
+			novaFrase = frase.toUpperCase();//tenta colocar uma String para maiuscula
+		}
+		catch(NullPointerException e)//ela faz a correção de um valor null para um conteúdo qualquer
+		{
+			//tratamento da exceção
+			System.out.println("\nA frase inicial está nula, para selecionar tal problema, "
+					+ "foi lhe atribuído um valor default...");
+			frase = "Frase vazia";
+			novaFrase = frase.toUpperCase();
+		}
+		System.out.println("\nFrase antiga: "+frase);
+		System.out.println("\nFrase nova: "+novaFrase);
+	}
+
+}
+
+
+
